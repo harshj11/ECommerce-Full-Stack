940f7c86a09b18cc8836b66729c346b82ae4beba
Harsh Jain <harshjain5454@gmail.com>
2022-08-20 17:50:54 +0530

    :sparkle:Authorize the user accessing the routes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,13 @@ const catchAsyncErrors = require("./catchAsyncErrors");
 
 const jwt = require('jsonwebtoken');
 
-const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+/**
+ * Authenticates the user.
+ * 
+ * @return an Error response if user is unauthenticated or any error occurs, 
+ * else calls the next middleware .
+ */
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     const { token } = req.cookies;
     if(!token)
         return next(new ErrorHandler(401, "Please login to access this resource."));
@@ -15,4 +21,17 @@ const isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
     next();
 });
 
-module.exports = isAuthenticatedUser;
+/**
+ * Authorizes the user.
+ * 
+ * @param {Array} roles
+ * 
+ * @return an Error response if the user is unauthorized, else calls the next middleware.
+ */
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role))
+            return next(new ErrorHandler(403, "Access not allowed!"));
+        next();
+    }
+};
