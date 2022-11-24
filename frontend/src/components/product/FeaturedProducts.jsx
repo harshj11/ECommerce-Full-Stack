753912741a3380adf011de715c23d0abf65647d6
Harsh Jain <harshjain5454@gmail.com>
2022-11-24 22:33:13 +0530

    :sparkle:CREATE product slice

--- frontend/src/components/product/FeaturedProducts.jsx
+++ frontend/src/components/product/FeaturedProducts.jsx
@@ -1,25 +1,28 @@
 import { Flex } from '@chakra-ui/react';
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { ProductCard } from './ProductCard';
 
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchProducts } from '../../features/products/productSlice';
+
 const FeaturedProducts = () => {
+
+    const dispatch = useDispatch();
+    const { products } = useSelector(state => state.products);
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [dispatch]);
+
     return (
         <Flex
             justifyContent='space-evenly'
             flexWrap='wrap'
             p={0}
         >
-            <ProductCard product={{ _id: 1, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 2, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 3, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 4, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 5, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 6, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 7, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 8, name: "Shirt Men" }} />
-            <ProductCard product={{ _id: 9, name: "Shirt Men" }} />
-            
+            { products && products.map(product => <ProductCard key={ product._id } product={product} />) }
         </Flex>
     )
 }
--- frontend/src/features/products/productSlice.js
+++ frontend/src/features/products/productSlice.js
@@ -1,27 +1,59 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+import axios from 'axios';
+
 export const productSlice = createSlice({
     name: 'products',
     initialState: {
-        products: []
+        loading: false,
+        products: [],
+        productsCount: 0,
+        errors: null
     },
     reducers: {
         loadingProducts: state => {
-            return {
-                loading: true,
-                products: []
-            }
+            state.loading = true;
         },
         setProducts: (state, { payload }) => {
             return {
                 loading: false,
                 products: payload.products,
-                productsCount: payload.productCount
+                productsCount: payload.productCount,
+                errors: null
             }
         },
-
+        setErrors: (state, { payload }) => {
+            return {
+                loading: false,
+                errors: payload
+            }
+        },
+        clearErrors: (state, { payload }) => {
+            return {
+                ...state, 
+                loading: false,
+                errors: null
+            }
+        }
     }
 });
 
-export const { loadingProducts, setProducts } = productSlice.actions
+export const fetchProducts = () => {
+    return async (dispatch, getState) => {
+        try {
+            dispatch(loadingProducts());
+            const { data } = await axios.get('/api/v1/products');
+            dispatch(setProducts(data));
+        } catch(error) {
+            dispatch(setErrors(error.response.data));
+        }
+    }
+}
+
+// Clearing Errors
+export const clearErrors = () => async (dispatch) => {
+    dispatch(clearErrors());
+}
+
+export const { loadingProducts, setProducts, setErrors } = productSlice.actions;
 export default productSlice.reducer;
